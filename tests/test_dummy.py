642a819864b9c4629eaf2cc0e5ce2d10966e26a1
Silas Kiehne <silki02>
2025-02-13 20:23:50 +0100

    add bsp test wurde hinzugefügt

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,16 @@
 def test_dummy():
     assert True  # Dieser Test wird immer erfolgreich sein
+from django_wetter_app/meineApp.views import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+
+def test_add_mixed_numbers():
+    assert add(-1, 1) == 0
+
+def test_add_zero():
+    assert add(0, 5) == 5
+    assert add(0, 0) == 0
